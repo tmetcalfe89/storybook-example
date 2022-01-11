4b2c9ac95e930a27e1c392f9979bb1dbce6083ae
Tim <tmetcalfe89@gmail.com>
2022-01-11 13:05:18 -0800

    Added auth

--- .storybook/run.js
+++ .storybook/run.js
@@ -1,8 +1,15 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
+
+const checkJwt = auth({
+  audience: "Storybook Example",
+  issuerBaseURL: `https://dev-2bman97t.us.auth0.com/`,
+});
 
 const app = express();
 
+app.use(checkJwt);
 app.use(express.static(path.join(__dirname, "../storybook-static")));
 
 const PORT = process.env.PORT || 3000;
