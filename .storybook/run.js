bec3af2817d51bfddd6632d4e4d9f1fe4fc3cdc2
Tim <tmetcalfe89@gmail.com>
2022-01-11 13:22:30 -0800

    fixing...

--- .storybook/run.js
+++ .storybook/run.js
@@ -1,15 +1,25 @@
+require("dotenv").config();
 const path = require("path");
 const express = require("express");
-const { auth } = require("express-oauth2-jwt-bearer");
-
-const checkJwt = auth({
-  audience: "Storybook Example",
-  issuerBaseURL: `https://dev-2bman97t.us.auth0.com/`,
-});
+const { auth } = require("express-openid-connect");
 
 const app = express();
 
-app.use(checkJwt);
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.STORYBOOK_AUTH_SECRET,
+    baseURL: process.env.STORYBOOK_AUTH_BASEURL,
+    clientID: process.env.STORYBOOK_AUTH_CLIENTID,
+    issuerBaseURL: process.env.STORYBOOK_AUTH_ISSUERBASEURL,
+  })
+);
+
+app.use((req, res, next) => {
+  req.oidc.isAuthenticated() ? next() : res.redirect("/login");
+});
+
 app.use(express.static(path.join(__dirname, "../storybook-static")));
 
 const PORT = process.env.PORT || 3000;
